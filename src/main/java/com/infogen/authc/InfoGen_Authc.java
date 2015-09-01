7f0278a0c025b63ca447e9d4773cff7f49b55408
larry <larrylv@juxinli.com>
2015-09-01 15:51:42 +0800

    update

---
+++ src/main/java/com/infogen/authc/InfoGen_Authc.java
@@ -0,0 +1,40 @@
+package com.infogen.authc;
+
+import org.apache.log4j.Logger;
+
+import com.infogen.authc.subject.Subject;
+import com.infogen.authc.subject.dao.Local_Subject_DAO;
+import com.infogen.authc.subject.dao.Subject_DAO;
+
+/**
+ * API认证框架的session本地缓存工具类,可以保存和获取subject
+ * 
+ * @author larry/larrylv@outlook.com/创建时间 2015年5月4日 下午2:11:06
+ * @since 1.0
+ * @version 1.0
+ */
+public class InfoGen_Authc {
+	private static final Logger LOGGER = Logger.getLogger(InfoGen_Authc.class.getName());
+	private static final ThreadLocal<Subject> thread_local_subject = new ThreadLocal<>();
+	public static Subject_DAO subject_dao = new Local_Subject_DAO();
+
+	public static Subject load(String x_access_token) {
+		return subject_dao.get(x_access_token);
+	}
+
+	public static Subject get() {
+		Subject subject = thread_local_subject.get();
+		if (subject == null) {
+			LOGGER.warn("没有找到当前线程存储的subject,检查是否有存入,或当前代码是否是在新创建的线程里执行的");
+		}
+		return subject;
+	}
+
+	public static void set(Subject subject) {
+		thread_local_subject.set(subject);
+	}
+
+	public static void save(Subject subject) {
+		subject_dao.save(subject);
+	}
+}
---
+++ src/main/java/com/infogen/authc/configuration/handle/impl/Authc_Properties_Handle_Main.java
@@ -0,0 +1,22 @@
+package com.infogen.authc.configuration.handle.impl;
+
+import com.infogen.authc.configuration.handle.Authc_Properties_Handle;
+
+/**
+ * 解析安全框架ini配置中[main] 基本配置的部分
+ * 
+ * @author larry/larrylv@outlook.com/创建时间 2015年6月19日 下午12:47:45
+ * @since 1.0
+ * @version 1.0
+ */
+public class Authc_Properties_Handle_Main extends Authc_Properties_Handle {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.infogen.authc.configuration.handle.properties_handle#handle(java.lang.String)
+	 */
+	@Override
+	public void handle(String line) {
+	}
+}
