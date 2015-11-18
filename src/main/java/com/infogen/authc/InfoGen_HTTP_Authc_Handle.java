034489bb0cc300ed9e379beda88734b22120a289
larry <larrylv@juxinli.com>
2015-11-18 18:28:10 +0800

    update

--- src/main/java/com/infogen/authc/InfoGen_HTTP_Authc_Handle.java
+++ src/main/java/com/infogen/authc/InfoGen_HTTP_Authc_Handle.java
@@ -95,10 +95,12 @@ public class InfoGen_HTTP_Authc_Handle {
 			InfoGen_Authc.set(subject);
 		} catch (InfoGen_Auth_Exception e) {
 			LOGGER.info("认证失败:", e);
+			response.sendRedirect("apage.jsp");
 			response.getWriter().write(Return.FAIL(CODE.authentication_fail, e).toJson());
 			return false;
 		} catch (Exception e) {
 			LOGGER.error("认证异常:", e);
+			response.sendRedirect("apage.jsp");
 			response.getWriter().write(Return.FAIL(CODE.error).toJson());
 			return false;
 		}
